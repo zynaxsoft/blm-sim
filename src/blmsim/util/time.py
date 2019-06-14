f492f9bc5977476becf7adb64111f274b8eac080
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2019-06-14 23:00:19 +0900

    move Time, Clock to util/time.py

---
+++ src/blmsim/util/time.py
@@ -0,0 +1,60 @@
+import math
+
+class Time:
+    def __init__(self, time=0.0, ticks=None, tick_value=0.01):
+        self.tick_value = tick_value
+        if ticks is not None:
+            self.ticks = ticks
+        else:
+            self.set_time(time)
+        self.epsilon = self.tick_value / 10.0
+        self.significant_order = int(-1*math.log10(self.tick_value))
+
+    def set_time(self, time):
+        self.ticks = int(math.ceil(float(time)/self.tick_value))
+
+    def get_time(self):
+        return self.ticks * self.tick_value
+
+    def is_zero(self):
+        return self.ticks == 0
+
+    def __repr__(self):
+        return f'{self.get_time():.{self.significant_order}f}'
+
+    def __lt__(self, other):
+        return self.ticks < other.ticks
+
+    def __gt__(self, other):
+        return self.ticks > other.ticks
+
+    def __add__(self, other):
+        return Time(self.ticks + other.ticks)
+
+    def __eq__(self, other):
+        return self.ticks == other.ticks
+
+    def __sub__(self, other):
+        return self.ticks - other.ticks
+
+class Clock(Time):
+    def __init__(self, time=0.0, ticks=None, tick_value=0.01):
+        super().__init__(time, ticks, tick_value)
+        self.hooks = []
+
+    def tick(self):
+        for h in self.hooks:
+            h.tock()
+        self.ticks += 1
+
+    def tock(self):
+        self.ticks = max(self.ticks-1, 0)
+
+    def reset(self):
+        self.ticks = 0
+
+    def hook(self, *hook):
+        self.hooks = self.hooks + list(hook)
+
+    def unhook(self, hook):
+        self.hooks.remove(hook)
--- src/simulation.py
+++ src/simulation.py
@@ -1,64 +1,4 @@
-import math
-
-class Time:
-    def __init__(self, time=0.0, ticks=None, tick_value=0.01):
-        self.tick_value = tick_value
-        if ticks is not None:
-            self.ticks = ticks
-        else:
-            self.set_time(time)
-        self.epsilon = self.tick_value / 10.0
-        self.significant_order = int(-1*math.log10(self.tick_value))
-
-    def set_time(self, time):
-        self.ticks = int(math.ceil(float(time)/self.tick_value))
-
-    def get_time(self):
-        return self.ticks * self.tick_value
-
-    def is_zero(self):
-        return self.ticks == 0
-
-    def __repr__(self):
-        return f'{self.get_time():.{self.significant_order}f}'
-
-    def __lt__(self, other):
-        return self.ticks < other.ticks
-
-    def __gt__(self, other):
-        return self.ticks > other.ticks
-
-    def __add__(self, other):
-        return Time(self.ticks + other.ticks)
-
-    def __eq__(self, other):
-        return self.ticks == other.ticks
-
-    def __sub__(self, other):
-        return self.ticks - other.ticks
-
-class Clock(Time):
-    def __init__(self, time=0.0, ticks=None, tick_value=0.01):
-        super().__init__(time, ticks, tick_value)
-        self.hooks = []
-
-    def tick(self):
-        for h in self.hooks:
-            h.tock()
-        self.ticks += 1
-
-    def tock(self):
-        self.ticks = max(self.ticks-1, 0)
-
-    def reset(self):
-        self.ticks = 0
-
-    def hook(self, *hook):
-        self.hooks = self.hooks + list(hook)
-
-    def unhook(self, hook):
-        self.hooks.remove(hook)
-clock = Clock()
+from blmsim.util.time import Clock, Time
 
 class DurationBuff:
     def __init__(self, name, duration, clock):
@@ -156,8 +96,9 @@ class Player:
         buff = DurationBuff('Swiftcast', 10, self.clock)
         buff.reduces_cast_time_by(100)
         self.receive_buff(buff)
-player = Player('Tomoyo', clock)
 
+clock = Clock()
+player = Player('Tomoyo', clock)
 player.leyline()
 while clock < Time(20):
 #   if not player.casting:
