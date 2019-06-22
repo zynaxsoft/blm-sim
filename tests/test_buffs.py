92433b4064d689026328e86b6d6f1764be51083f
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2019-06-22 22:11:10 +0900

    pep8 test_buffs.py

--- tests/test_buffs.py
+++ tests/test_buffs.py
@@ -1,21 +1,25 @@
+""" Test buff related """
 import unittest
-from blmsim.util.time import Clock, Time
-from blmsim.skills import *
-from blmsim.player import Player
-from blmsim.buffs import *
+
+from blmsim import buffs
 
 class TestBuffs(unittest.TestCase):
 
     def test_buff_check_member_astral_umbral(self):
-        a_list = [AstralFire(1), AstralFire(2), AstralFire(3)]
-        u_list = [UmbralIce(1), UmbralIce(2), UmbralIce(3)]
+        a_list = [buffs.AstralFire(1),
+                  buffs.AstralFire(2),
+                  buffs.AstralFire(3)]
+        u_list = [buffs.UmbralIce(1),
+                  buffs.UmbralIce(2),
+                  buffs.UmbralIce(3)]
         for i in range(1, 4):
-            self.assertTrue(AstralFire(i) in a_list)
-            self.assertFalse(AstralFire(i) in u_list)
-            self.assertTrue(UmbralIce(i) in u_list)
-            self.assertFalse(UmbralIce(i) in a_list)
+            self.assertTrue(buffs.AstralFire(i) in a_list)
+            self.assertFalse(buffs.AstralFire(i) in u_list)
+            self.assertTrue(buffs.UmbralIce(i) in u_list)
+            self.assertFalse(buffs.UmbralIce(i) in a_list)
         self.assertTrue('Astral or Umbral' in a_list)
         self.assertTrue('Astral or Umbral' in u_list)
+
 
 if __name__ == '__main__':
     unittest.main()
